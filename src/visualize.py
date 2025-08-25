fb0354e58fa75ea1b36bc1232710e4c6ad0afb29
MrFaruk0 <akdago672@gmail.com>
2025-08-26 00:07:52 +0300

    plot functions for gyro and magnetometer added

--- src/visualize.py
+++ src/visualize.py
@@ -1,6 +1,8 @@
 import matplotlib.pyplot as plt
+from scipy import signal
+import numpy as np
 
-def plot_exercise(df, exercise_label, subject=None, unit=None):
+def plot_acc_exercise(df, exercise_label, subject=None, unit=None):
     subset = df[df['exercise'] == exercise_label]
     if subject:
         subset = subset[subset['subject'] == subject]
@@ -14,9 +16,45 @@ def plot_exercise(df, exercise_label, subject=None, unit=None):
     plt.plot(subset['time'], subset['acc_z'], label='Acc Z')
     plt.legend()
     plt.title(f'{exercise_label} ivme değerleri')
-    if subject: title += f" - {subject}"
-    if unit: title += f" - {unit}"
-    plt.title(title)
     plt.xlabel('Zaman (s)')
     plt.ylabel('ivme (m/s²)')
+    plt.show()
+
+def plot_gyro_exercise(df, exercise_label, subject=None, unit=None):
+    subset = df[df['exercise'] == exercise_label]
+    if subject:
+        subset = subset[subset['subject'] == subject]
+    if unit:
+        subset = subset[subset['unit'] == unit]
+
+    b, a = signal.butter(4, 0.1, btype='low')
+    filtered_gyro_x = signal.filtfilt(b, a, subset['gyr_x'])
+    filtered_gyro_y = signal.filtfilt(b, a, subset['gyr_y'])
+    filtered_gyro_z = signal.filtfilt(b, a, subset['gyr_z'])
+
+    plt.figure(figsize=(10,5))
+    plt.plot(subset['time'], filtered_gyro_x, label='Gyr X')
+    plt.plot(subset['time'], filtered_gyro_y, label='Gyr Y')
+    plt.plot(subset['time'], filtered_gyro_z, label='Gyr Z')
+    plt.legend()
+    plt.title(f'{exercise_label} jiroskop değerleri (filtrelenmiş)')
+    plt.xlabel('Zaman (s)')
+    plt.ylabel('Açı Hızı (°/s)')
+    plt.show()
+
+def plot_mag_exercise(df, exercise_label, subject=None, unit=None):
+    subset = df[df['exercise'] == exercise_label]
+    if subject:
+        subset = subset[subset['subject'] == subject]
+    if unit:
+        subset = subset[subset['unit'] == unit]
+
+    plt.figure(figsize=(10,5))
+    plt.plot(subset['time'], subset['mag_x'], label='Mag X')
+    plt.plot(subset['time'], subset['mag_y'], label='Mag Y')
+    plt.plot(subset['time'], subset['mag_z'], label='Mag Z')
+    plt.legend()
+    plt.title(f'{exercise_label} manyetometre değerleri')
+    plt.xlabel('Zaman (s)')
+    plt.ylabel('Manyetik Alan (µT)')
     plt.show()
